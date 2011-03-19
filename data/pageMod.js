521b8bc23a1257a6921ea00244f99917f66fde3d
Hernán Rodríguez Colmeiro <colmeiro@gmail.com>
2011-03-18 21:25:33 -0300

    New feature, show current elapsed time / buffered song on song display (Issue 24) Changed styles of the songDisplay, now looks nicer

--- data/pageMod.js
+++ data/pageMod.js
@@ -16,6 +16,7 @@ onMessage = function onMessage(action) {
 };
 
 try{
+  var flag = false;
   window.unloadListener = function(e){
     postMessage({"action":"unload"});
     return true;
@@ -23,14 +24,27 @@ try{
   
   window.nowPlayingListener = function(song){
     postMessage({"action":"nowPlaying", "song":song})
+    if (!flag) {
+      GS.player.player.setPlaybackStatusCallback(
+        "function(b){GS.Controllers.PlayerController.instance().playerStatus(b); progressListener(b);}"
+      );
+    }
   }
   
   window.stoppedListener = function(song){
     postMessage({"action":"stopped"})
   }
   
+  window.progressListener = function(event){
+    postMessage({
+      "action":"songProgress",
+      "position": Math.ceil(event.position/event.duration*100),
+      "buffered": Math.ceil(event.bytesLoaded/event.bytesTotal*100)
+    });
+  }
+  
   jQuery(window).unload(unloadListener);
   jQuery.subscribe("gs.player.nowplaying", nowPlayingListener);
   jQuery.subscribe("gs.player.stopped", stoppedListener);
-  
+
 } catch(e) { };
---
+++ data/ui/songDisplay.js
@@ -0,0 +1,21 @@
+onMessage = function onMessage(msg) {
+  switch(msg.action){
+    
+    case 'songProgress':
+      updateProgressMeters(msg.position, msg.buffered);
+      break
+    
+    case 'nowPlaying':
+      document.querySelector('#songDisplay').innerHTML =
+        msg.song.SongName + ' - ' + msg.song.ArtistName
+      stopScrolling();  //Stop the posible scrolling of the last song
+      startScrolling(); //if needed, start scrolling the text
+      break
+    
+    case 'notPlaying':
+      document.querySelector('#songDisplay').innerHTML =
+        'Not Playing';
+      //stopScrolling();
+      break
+  }
+}
