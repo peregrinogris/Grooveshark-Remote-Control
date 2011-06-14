bc7ae9d8e5b8699b9ec603c2288dc1873e70ff2d
Hernán Rodríguez Colmeiro <colmeiro@gmail.com>
2011-06-14 00:05:41 -0700

    Fixed Issue 28 about wrong icons for skip buttons Fixed whitespace and deprecated comments

--- data/pageMod.js
+++ data/pageMod.js
@@ -28,10 +28,10 @@ try{
     postMessage({"action":"unload"});
     return true;
   }
-  
+
   //flag to know when to tap the communication between the UI and the player
   var wireTapped = false;
-  
+
   window.nowPlayingListener = function(song){
     if(song.ArtistName) {
       postMessage({"action":"nowPlaying", "song":song, "notify": true});
@@ -43,11 +43,11 @@ try{
       }
     }
   }
-  
+
   window.stoppedListener = function(song){
     postMessage({"action":"stopped", "songsQueued":song.AlbumID > 0})
   }
-  
+
   window.progressListener = function(event){
     postMessage({
       "action":"songProgress",
@@ -55,11 +55,11 @@ try{
       "buffered": Math.ceil(event.bytesLoaded/event.bytesTotal*100)
     });
   }
-  
+
   window.playingListener = function(event){
       postMessage({"action":"nowPlaying", "song":event.activeSong, "notify": false});
   }
-  
+
   jQuery(window).unload(unloadListener);
   jQuery.subscribe("gs.player.nowplaying", nowPlayingListener);
   jQuery.subscribe("gs.player.playing", playingListener);
