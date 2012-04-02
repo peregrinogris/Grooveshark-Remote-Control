d3fa02ca71c9aa90ee15c71e02f725b4e2f6e748
Hernán Rodríguez Colmeiro <colmeiro@gmail.com>
2012-04-02 18:11:50 -0300

    Show album and song info in album panel - Fix #29

--- data/ui/albumArt.js
+++ data/ui/albumArt.js
@@ -1,3 +1,11 @@
+var $ = function (selector) {
+  return document.querySelector(selector);
+}
+
 self.port.on("album", function(msg) {
-  document.getElementById('albumArt').src = msg
+  $('#albumArt').src = msg.albumArtURL
+  $('#songName').innerHTML = msg.songName;
+  $('#artistName').innerHTML = msg.artistName;
+  $('#albumName').innerHTML = msg.albumName;
+  $('.background').style.minHeight = $('.songInfo').clientHeight+'px';
 });
